9015b619ea0bba724f8197f9b5b938b3a7b84fad
Stefan Ilie <ilie.stefan92@yahoo.com>
2015-05-24 20:10:15 +0300

    Form 90% done.
    Needs submit button and link to model.

--- VacationLocation/VacationLocation/Models/FormModel.cs
+++ VacationLocation/VacationLocation/Models/FormModel.cs
@@ -11,6 +11,12 @@ namespace VacationLocation.Models
 
         public string hasKids { get; set; }
 
+        public string birthPlace { get; set; }
+
+        public string climate { get; set; }
+
+        public string residencePlace { get; set; }
+
         public int selectedID { get; set; }
 
         public System.Web.Mvc.SelectList status;
